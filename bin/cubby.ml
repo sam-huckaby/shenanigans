9b25752f9e5f6b7e310c01baaa61cb5b743caa2f
Sam Huckaby <samhuckaby@gmail.com>
2025-06-12 22:29:27 -0400

    Made some progress on a dynamic box but ran into a unicode issue

--- bin/cubby.ml
+++ bin/cubby.ml
@@ -2,13 +2,29 @@ let clear_screen_ansi () : unit =
   print_string "\x1b[2J\x1b[H";
   flush stdout
 
+let rec count_width lines =
+  match lines with
+  | [] -> 0
+  | [ line ] -> String.length line
+  | line :: rest -> Int.max (String.length line) (count_width rest)
+
+let cubby_hole contents =
+  (** The width of the provided contents in bytes? *)
+  let width = count_width contents in
+  (* BUG: the horizontal line is not a valid character, it must be treated as a string *)
+  print_endline ("\x1b[31m┏" ^ (String.make width '━') ^ "┓\x1b[0m");
+  print_endline (Int.to_string width);
+  print_endline "\x1b[31m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\x1b[0m"
+
 (* \x1b[0m is the closing tag *)
+
+(* \x1b[1m\x1b[92mHello, Cubby\x1b[0m *)
 
 (* Box drawing characters are a thing: https://en.wikipedia.org/wiki/Box-drawing_characters *)
 let render () =
-  print_endline "\x1b[31m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\x1b[0m";
-  print_endline "\x1b[1m \x1b[92mHello, Cubby\x1b[0m";
-  print_endline "\x1b[31m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\x1b[0m"
+  cubby_hole [
+    "Hello, Cubby"
+  ]
 
 let () = 
   clear_screen_ansi ();
